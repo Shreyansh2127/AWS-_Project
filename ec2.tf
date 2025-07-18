a8237703e0c3f991d20c1e3c9fed8ea353dc8ad3
ShreyanshSingh21 <shreyansh2127@gmail.com>
2025-07-18 10:57:37 +0530

    Initial Commit with Terrafrom

---
+++ ec2.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "web" {
+  ami           = "ami-0c02fb55956c7d316" # Ubuntu 20.04 LTS
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_a.id
+  associate_public_ip_address = true
+  tags = {
+    Name = "WebAppServer"
+  }
+}
---
+++ rds.tf
@@ -0,0 +1,11 @@
+
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-b"
+  }
+}
